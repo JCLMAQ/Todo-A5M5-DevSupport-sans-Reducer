62d372719eec52eeef0098efa791d4f7a702e390
JCM <jcl.maquinay@gmail.com>
2018-04-10 15:18:42 +0200

    Some tests

--- backend/models/main/methods/Todo/Todo-events.js
+++ backend/models/main/methods/Todo/Todo-events.js
@@ -2,9 +2,11 @@
 model.Todo.events.init = function(event) {
  this.done = false;
 };
-model.Todo.events.restrict = function(event) {
-	return ds.Todo.query("userID == 1", directory.currentUser.ID);
-};
-model.Todo.events.save = function(event) {
-	this.userID = directory.currentUser.ID;
-};
+//model.Todo.events.restrict = function(event) {
+//	return ds.Todo.query("userID == 1", directory.currentUser.ID);
+	// TODO : add the selection on the attributed user or the "public" todo
+//};
+//model.Todo.events.save = function(event) {
+//	debugger;
+//	this.userID = directory.currentUser.ID;
+//};
